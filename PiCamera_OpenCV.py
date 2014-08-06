097055a6d0d6b039700bbc0de45b0b39f933bda4
richhohart <richhohart@gmail.com>
2014-08-06 13:21:41 -0500

    adding in OpenCV window

--- PiCamera_OpenCV.py
+++ PiCamera_OpenCV.py
@@ -9,7 +9,18 @@ import cv2.cv as cv
 with picamera.PiCamera() as camera:
     with picamera.array.PiRGBArray(camera) as stream:
         camera.resolution = (1024, 768)
-        camera.start_preview()
-        time.sleep(2)
-        camera.capture(stream, 'rgb')
-        print(stream.array.shape)
+        #camera.start_preview()
+        
+        #time.sleep(2)
+        #camera.capture(stream, 'rgb')
+        cv.NamedWindow("camera", 1)
+
+        #capture = cv.CaptureFromCAM(0)
+
+    while True:
+        #img = cv.QueryFrame(capture)
+        cv.ShowImage("camera", stream)
+        if cv.WaitKey(10) == 27: #'Esc' Key 
+            break
+        #cv.DestroyAllWindows()
+        #print(stream.array.shape)
