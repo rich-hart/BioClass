c703e903bb85e8d2da7a5d074032b2be117a1627
richhohart <richhohart@gmail.com>
2014-08-06 13:50:21 -0500

    changing data from static to streaming

--- PiCamera_OpenCV.py
+++ PiCamera_OpenCV.py
@@ -10,10 +10,7 @@ with picamera.PiCamera() as camera:
     #camera.start_preview()
     #time.sleep(2)
     camera.capture(stream, format='jpeg')
-# Construct a numpy array from the stream
-data = np.fromstring(stream.getvalue(), dtype=np.uint8)
-# "Decode" the image from the array, preserving colour
-image = cv2.imdecode(data, 1)
+
 # OpenCV returns an array with data in BGR order. If you want RGB instead
 # use the following...
 #image = image[:, :, ::-1]
@@ -24,7 +21,10 @@ cv.NamedWindow("camera", 1)
 
 while True:
     #img = cv.QueryFrame(capture)
-    
+    # Construct a numpy array from the stream
+    data = np.fromstring(stream.getvalue(), dtype=np.uint8)
+    # "Decode" the image from the array, preserving colour
+    image = cv2.imdecode(data, 1)
     cv.ShowImage("camera", cv.fromarray(image))
     if cv.WaitKey(10) == 27:
         break
