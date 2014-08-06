1c69c6c987e5200aff54eec5c426cd36c46660be
richhohart <richhohart@gmail.com>
2014-08-06 13:52:14 -0500

    streaming attempt

--- PiCamera_OpenCV.py
+++ PiCamera_OpenCV.py
@@ -4,12 +4,10 @@ import picamera
 import cv2
 import numpy as np
 import cv2.cv as cv
-# Create the in-memory stream
-stream = io.BytesIO()
-with picamera.PiCamera() as camera:
+
     #camera.start_preview()
     #time.sleep(2)
-    camera.capture(stream, format='jpeg')
+    
 
 # OpenCV returns an array with data in BGR order. If you want RGB instead
 # use the following...
@@ -18,15 +16,17 @@ with picamera.PiCamera() as camera:
 cv.NamedWindow("camera", 1)
 
 #capture = cv.CaptureFromCAM(0)
-
-while True:
-    #img = cv.QueryFrame(capture)
-    # Construct a numpy array from the stream
-    data = np.fromstring(stream.getvalue(), dtype=np.uint8)
-    # "Decode" the image from the array, preserving colour
-    image = cv2.imdecode(data, 1)
-    cv.ShowImage("camera", cv.fromarray(image))
-    if cv.WaitKey(10) == 27:
-        break
+stream = io.BytesIO()
+with picamera.PiCamera() as camera:
+    while True:
+        camera.capture(stream, format='jpeg')
+        #img = cv.QueryFrame(capture)
+        # Construct a numpy array from the stream
+        data = np.fromstring(stream.getvalue(), dtype=np.uint8)
+        # "Decode" the image from the array, preserving colour
+        image = cv2.imdecode(data, 1)
+        cv.ShowImage("camera", cv.fromarray(image))
+        if cv.WaitKey(10) == 27:
+            break
 cv.DestroyAllWindows()
 
